ae9e183e61a3ecd4c12fb812712060a4c7d841f4
bader <bader5uk@hotmail.com>
2024-09-23 11:51:35 +0400

    fixed slightly

--- stringRotation2.java
+++ stringRotation2.java
@@ -8,13 +8,17 @@ public class stringRotation2 {
         Scanner str2 = new Scanner(System.in);
         System.out.println("Enter second word");
         String secondString = str2.next();
-
         char[] array = firstString.toCharArray();
         char[] array2 = secondString.toCharArray();
-        if (array[array.length-1] == array2[0]) {
-            System.out.println("Is a rotation of ");
-        }
-        else {
+        if (array[array.length - 1] == array2[0]) {
+            for (int i = 0; i < firstString.length(); i++) {
+                firstString = firstString.substring(1) + firstString.charAt(0);
+                array = firstString.toCharArray();
+                array2 = secondString.toCharArray();
+                System.out.println(array);
+                System.out.println("Is a rotation of ");
+            }
+        } else {
             System.out.println("Not a rotation");
         }
     }
